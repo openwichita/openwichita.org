4bbaa5497e0b8b36c5cc33a38671870ad12eabe0
Seth Etter <sethetter@gmail.com>
2015-05-14 16:11:21 -0500

    Temporary test URL

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
   // When running Ghost in the wild, use the production environment
   // Configure your URL and mail settings here
   production: {
-    url: 'http://openwichita.com',
+    url: 'http://test.openwichita.com',
     mail: {},
     database: {
       client: 'pg',
