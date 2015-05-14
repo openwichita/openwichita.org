a7ab03a04539949461ccc78fc30111e288e5aeb8
Seth Etter <sethetter@gmail.com>
2015-05-14 15:49:54 -0500

    Use postgres in production for dokku

--- config.js
+++ config.js
@@ -3,89 +3,93 @@
 // Documentation can be found at http://support.ghost.org/config/
 
 var path = require('path'),
-    config;
+config;
 
 config = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
-    production: {
-        url: 'http://openwichita.com',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '0.0.0.0',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: process.env.PORT
-        }
+  // ### Production
+  // When running Ghost in the wild, use the production environment
+  // Configure your URL and mail settings here
+  production: {
+    url: 'http://openwichita.com',
+    mail: {},
+    database: {
+      client: 'pg',
+      connection: {
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        charset: 'utf8'
+      },
+      debug: false
     },
 
-    // ### Development **(default)**
-    development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blogs published URL.
-        url: 'http://localhost:2368',
-
-        // Example mail config
-        // Visit http://support.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
-
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-            },
-            debug: false
-        },
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
-        },
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
-    },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
-    testing: {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-test.db')
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
+    server: {
+      // Host to be passed to node's `net.Server#listen()`
+      host: '0.0.0.0',
+      // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+      port: process.env.PORT
     }
+  },
+
+  // ### Development **(default)**
+  development: {
+    // The url to use when providing links to the site, E.g. in RSS and email.
+    // Change this to your Ghost blogs published URL.
+    url: 'http://localhost:2368',
+
+    // Example mail config
+    // Visit http://support.ghost.org/mail for instructions
+    // ```
+    //  mail: {
+    //      transport: 'SMTP',
+    //      options: {
+    //          service: 'Mailgun',
+    //          auth: {
+    //              user: '', // mailgun username
+    //              pass: ''  // mailgun password
+    //          }
+    //      }
+    //  },
+    // ```
+
+    database: {
+      client: 'sqlite3',
+      connection: {
+        filename: path.join(__dirname, '/content/data/ghost-dev.db')
+      },
+      debug: false
+    },
+    server: {
+      // Host to be passed to node's `net.Server#listen()`
+      host: '127.0.0.1',
+      // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+      port: '2368'
+    },
+    paths: {
+      contentPath: path.join(__dirname, '/content/')
+    }
+  },
+
+  // **Developers only need to edit below here**
+
+  // ### Testing
+  // Used when developing Ghost to run tests and check the health of Ghost
+  // Uses a different port number
+  testing: {
+  url: 'http://127.0.0.1:2369',
+  database: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, '/content/data/ghost-test.db')
+    }
+  },
+  server: {
+    host: '127.0.0.1',
+    port: '2369'
+  },
+  logging: false
+}
 
 };
 
