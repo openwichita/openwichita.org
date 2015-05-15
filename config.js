8169c3ba81b2f1e19826329a81f7bfa6a77e1030
Seth Etter <sethetter@gmail.com>
2015-05-15 08:37:00 -0500

    Ignore all databases!

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
   // When running Ghost in the wild, use the production environment
   // Configure your URL and mail settings here
   production: {
-    url: 'http://test.openwichita.com',
+    url: 'http://openwichita.com',
     mail: {},
     database: {
       client: 'pg',
